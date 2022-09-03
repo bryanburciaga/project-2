f79096f13446ca7733449a1a7a2a3f482f721d8d
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-09-02 19:43:25 -0600

    Port set up working

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+PORT = process.env.PORT
+// const DATABASE_URI = process.env.DATABASE_URI;
+
+const app = express();
+
+// mongoose.connect(DATABASE_URI)
+
+// const db = mongoose.connecting;
+
+// db.on('connected to', () => console.log('Connected to MongoDB'));
+// db.on('error,', (err) => console.log('MongoDB Error'));
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`I'm listening on port: ${PORT}`);
+});
